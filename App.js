b823f132cbb4b424803297865372661b847969d2
lathsamee <giftsforam@gmail.com>
2020-07-08 16:30:25 +0700

    ++ Add ++ new page

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import {View} from 'react-native';
 import ScrollY from './src/components/scrollY';
 import Header from './src/components/scrollHeader';
 import HeaderY from './src/components/scrollYHeader';
+import HeaderTabs from './src/components/UberEats';
 const App = (props) => {
   return (
     <View style={{flex: 1}}>
-      <HeaderY />
+      <HeaderTabs />
     </View>
   );
 };
