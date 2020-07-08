3ec6ecffecbf7cf15609e8377e0bd02fb5bf338f
lathsamee <giftsforam@gmail.com>
2020-07-08 15:34:16 +0700

    ++ fixed ++ scroll view array 0

--- src/components/scrollYHeader.js
+++ src/components/scrollYHeader.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {View, Text, Image, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Animated,
+  StyleSheet,
+} from 'react-native';
 
-const HeaderY = (props) => {
+const scrollYHeader = (props) => {
   const NAME_ = 'Papaya PokPok';
   const imgHeader = require('../image/feature_graphic.png');
   const image = [
@@ -13,34 +20,90 @@ const HeaderY = (props) => {
     {id: 6, uri: require('../image/shop1.png')},
     {id: 1, uri: require('../image/goteddy-bg.png')},
   ];
+
+  // * animated
+  const scrollY = new Animated.Value(0);
+
+  // * header opacity
+  const opacityContent = scrollY.interpolate({
+    inputRange: [0, 140],
+    outputRange: [1, 0],
+    extrapolate: 'clamp',
+    useNativeDriver: true,
+  });
+  // * header background color
+  const backgroundHeaderColor = scrollY.interpolate({
+    inputRange: [0, 140],
+    outputRange: ['rgba(255,255,255,0.0)', '#2ea9'],
+    extrapolate: 'clamp',
+    useNativeDriver: true,
+  });
+  // * opacity title
+  const headerTitleOpacity = scrollY.interpolate({
+    inputRange: [0, 50, 105, 150],
+    outputRange: [0, 0.1, 0.5, 1],
+    extrapolate: 'clamp',
+    useNativeDriver: true,
+  });
+  const contentMarginTop = scrollY.interpolate({
+    inputRange: [0, 120],
+    outputRange: [120, 0],
+    extrapolate: 'clamp',
+    useNativeDriver: true,
+  });
+  const contentTop = scrollY.interpolate({
+    inputRange: [0, 140],
+    outputRange: [10, 0],
+    extrapolate: 'clamp',
+    useNativeDriver: true,
+  });
+
   return (
     <View style={{flex: 1}}>
-      <Image
+      <Animated.Image
         style={{
           height: 200,
           width: '100%',
           top: 0,
           alignSelf: 'center',
           position: 'absolute',
+          opacity: opacityContent,
         }}
         source={imgHeader}
       />
-      <View style={{position: 'absolute', left: 7}}>
-        <FontAwesome5 name={'angle-left'} brand size={30} />
-      </View>
-      <View style={{position: 'absolute', right: 7}}>
-        <FontAwesome5 name={'calendar-plus'} brand size={30} />
-      </View>
-      <Text
-        style={{textAlign: 'center', justifyContent: 'center', fontSize: 16}}>
-        {NAME_}
-      </Text>
+      <Animated.View
+        style={{
+          height: 70,
+          backgroundColor: backgroundHeaderColor,
+          width: '100%',
+          alignItem: 'center',
+          justifyContent: 'center',
+          zIndex: 200,
+          elevation: 200,
+        }}>
+        <View style={{position: 'absolute', left: 7}}>
+          <FontAwesome5 name={'angle-left'} brand size={30} />
+        </View>
+        <View style={{position: 'absolute', right: 7}}>
+          <FontAwesome5 name={'calendar-plus'} brand size={30} />
+        </View>
+        <Animated.Text
+          style={{
+            textAlign: 'center',
+            justifyContent: 'center',
+            fontSize: 16,
+            opacity: headerTitleOpacity,
+          }}>
+          {NAME_}
+        </Animated.Text>
+      </Animated.View>
 
-      <View
+      <Animated.View
         style={{
           position: 'absolute',
-          top: 140,
-          zIndex: 1000,
+          opacity: opacityContent,
+          top: contentMarginTop,
+          zIndex: 100,
           borderRadius: 20,
           height: 120,
           width: '90%',
@@ -54,8 +117,8 @@ const HeaderY = (props) => {
           },
           shadowOpacity: 0.22,
           shadowRadius: 2.22,
-
           elevation: 3,
+          transform: [{translateY: contentTop}],
         }}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
           <Text>{NAME_}</Text>
@@ -69,12 +132,26 @@ const HeaderY = (props) => {
           <FontAwesome5 name={'facebook'} />
           <Text>Promotion</Text>
         </View>
-      </View>
+      </Animated.View>
 
-      <ScrollView>
+      <Animated.ScrollView
+        stickyHeaderIndices={[0]}
+        overScrollMode={'never'}
+        style={{zIndex: 10}}
+        scrollEventThrottle={16}
+        onScroll={Animated.event([
+          {nativeEvent: {contentOffset: {y: scrollY}}},
+        ])}>
+        <View style={{top: 220, backgroundColor: 'green'}}>
+          <Text>
+            Open the Rendering tab and use the new Emulate vision deficiencies
+            feature to get a better idea of how people with different types of
+            vision deficiencies experience your site
+          </Text>
+        </View>
         {image.map((value) => {
           return (
-            <View key={value.id} style={{height: 300, margin: 10, top: 500}}>
+            <View key={value.id} style={{height: 300, margin: 10, top: 250}}>
               <Image
                 source={value.uri}
                 style={{flex: 1, width: null, height: null, borderRadius: 20}}
@@ -82,8 +159,14 @@ const HeaderY = (props) => {
             </View>
           );
         })}
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   );
 };
-export default HeaderY;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+export default scrollYHeader;
