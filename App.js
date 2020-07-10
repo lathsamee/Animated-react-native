aa89a3937c5bfc2aebbb9feb4128f2a7775263f6
lathsamee <giftsforam@gmail.com>
2020-07-10 16:23:07 +0700

    last end code

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import ScrollY from './src/components/scrollY';
 import Header from './src/components/scrollHeader';
 import HeaderY from './src/components/scrollYHeader';
 import HeaderTabs from './src/components/tabs/UberEats';
 import Exam from './src/components/example';
+import Type from './src/components/typeScript/UberEats';
 const App = (props) => {
   return (
     <View style={{flex: 1}}>
-      <Exam />
+      <SafeAreaProvider>
+        <Type />
+      </SafeAreaProvider>
     </View>
   );
 };
